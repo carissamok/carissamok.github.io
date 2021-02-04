ce947beec8c365c3b815bf41841e52280c5d13a6
carissamok <61439305+carissamok@users.noreply.github.com>
2021-02-03 22:38:01 -0500

    Add files via upload

---
+++ react-spotify-player-master/src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "a4bcdd8621fb4efbbfd97e3b8ca94081";
+export const redirectUri = "https://carissamok.github.io";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
